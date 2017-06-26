8779c734a2e71deba7c402ef5914328f78bc1011
sgoodm <sgoodman@aiddata.org>
2017-06-26 16:30:49 -0400

    Add canal data, script to aggregate lines/polygons (and output) and distance script

---
+++ aggregate_shapes.py
@@ -0,0 +1,67 @@
+
+import os
+import glob
+import json
+import fiona
+
+
+
+project_dir = "/sciclone/aiddata10/REU/projects/afghanistan_gie"
+
+area_path_list = glob.glob(
+    "{0}/mapping/mapping_uploads/*/*_poly*.shp".format(project_dir))
+canal_path_list = glob.glob(
+    "{0}/mapping/mapping_uploads/*/*_line*.shp".format(project_dir))
+
+
+# make sure all projects have area and canal
+assert len(area_path_list) == len(canal_path_list) == 87
+
+
+# -----------------------------------------------------------------------------
+
+# combine all lines (no project information needed)
+
+
+all_canal_features = []
+for path in canal_path_list:
+    project_id = os.path.basename(path)[:4]
+    with fiona.open(path, 'r') as feat_src:
+        for feature in feat_src:
+            feature['properties']['project_id'] = project_id
+            all_canal_features.append(feature)
+
+canal_geo = {
+  "type": "FeatureCollection",
+  "features": all_canal_features
+}
+
+canal_path = os.path.expanduser(
+    "~/git/afghanistan_gie/canal_data/canal_lines.geojson")
+canal_file = open(canal_path, "w")
+json.dump(canal_geo, canal_file)
+canal_file.close()
+
+
+
+all_area_features = []
+for path in area_path_list:
+    project_id = os.path.basename(path)[:4]
+    with fiona.open(path, 'r') as feat_src:
+        for feature in feat_src:
+            feature['properties']['project_id'] = project_id
+            all_area_features.append(feature)
+
+
+
+area_geo = {
+  "type": "FeatureCollection",
+  "features": all_area_features
+}
+
+area_path = os.path.expanduser(
+    "~/git/afghanistan_gie/canal_data/canal_polygons.geojson")
+area_file = open(area_path, "w")
+json.dump(area_geo, area_file)
+area_file.close()
+
--- distance_to_canals/build_dist_raster.py
+++ distance_to_canals/build_dist_raster.py
@@ -1,58 +1,47 @@
 
 
+
+import os
+
 from distancerasters import build_distance_array, rasterize, export_raster
 
 
 # -----------------------------------------------------------------------------
 
 from affine import Affine
-import numpy as np
+# import numpy as np
+import fiona
 
-names = [
-    'africa', 'americas', 'asia', 'europe', 'oceania-east', 'oceania-west'
-]
+pixel_size = 0.0002695
 
-pixel_size = 0.01
+canal_path = os.path.expanduser(
+    "~/git/afghanistan_gie/canal_data/canal_lines.geojson")
 
-xmin = -180
-xmax = 180
-ymin = -90
-ymax = 90
-
-affine = Affine(pixel_size, 0, xmin,
-                0, -pixel_size, ymax)
-
-shape = (int((ymax-ymin)/pixel_size), int((xmax-xmin)/pixel_size))
-
-roads = np.zeros(shape, dtype='byte')
-
-for n in names:
-    path = "{0}/{1}".format("/sciclone/aiddata10/REU/raw/groads",
-                            "groads-v1-{0}-shp/gROADS-v1-{0}.shp".format(n))
-    rv_array, _ = rasterize(path, affine=affine, shape=shape)
-    roads = roads | rv_array
+with fiona.open(canal_path) as canal_src:
+    bounds = canal_src.bounds
 
 
-roads_output_raster_path = "/sciclone/aiddata10/REU/data/rasters/external/global/distance_to/groads/binary/groads_binary.tif"
+rv_array, affine = rasterize(canal_path, pixel_size=pixel_size, bounds=bounds)
 
-export_raster(roads, affine, roads_output_raster_path)
+
+binary_raster_path = os.path.expanduser(
+    "~/git/afghanistan_gie/raster/binary_canals.tif")
+
+export_raster(rv_array, affine, binary_raster_path)
 
 
 # -----------------------------------------------------------------------------
 
-# import rasterio
-# roads_src = rasterio.open(roads_output_raster_path)
-# roads = roads_src.read()[0]
-# affine = roads_src.affine
 
-distance_output_raster_path = "/sciclone/aiddata10/REU/data/rasters/external/global/distance_to/groads/groads_distance.tif"
-
+distance_raster_path = os.path.expanduser(
+    "~/git/afghanistan_gie/raster/distance_canals.tif")
 
 def raster_conditional(rarray):
     return (rarray == 1)
 
-dist = build_distance_array(roads, affine=affine,
-                            output=distance_output_raster_path,
+dist = build_distance_array(rv_array, affine=affine,
+                            output=distance_raster_path,
                             conditional=raster_conditional)
+
 
 
